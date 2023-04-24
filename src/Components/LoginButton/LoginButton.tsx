c5138e539f0b2d1b228c536a39c70a12f2f45f10
renanwilson <renan.alves.ra16@gmail.com>
2023-04-24 16:24:33 -0300

    feat: add dot env

--- src/Components/LoginButton/LoginButton.tsx
+++ src/Components/LoginButton/LoginButton.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 
 import { Button, Link, Logo } from "./styles";
-import Image from "next/image";
 
 export function LoginButtton() {
-  const CLIENT_ID = "644c94c28962489b83748755d29459c1";
   const REDIRECT_URI = "http://localhost:3000/list-track";
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
   const RESPONSE_TYPE = "token";
@@ -13,7 +11,7 @@ export function LoginButtton() {
 
   return (
     <Link
-      href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`}
+      href={`${AUTH_ENDPOINT}?client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`}
     >
       <Button>
         Entrar <Logo src="/images/logoSpotifyIcon.png" alt="logo" />
